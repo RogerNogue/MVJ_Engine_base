d220536c71f98e911ca4498fb5429479e5bf3012
RogerNogue <rogernogueb@gmail.com>
2018-10-31 21:33:47 +0100

    module model loader loads the model

--- ModuleModelLoader.cpp
+++ ModuleModelLoader.cpp
@@ -3,11 +3,9 @@
 #include "ModuleMenu.h"
 #include <assimp/cimport.h>
 #include <assimp/postprocess.h>
-#include <assimp/scene.h>
+#include "GL/glew.h"
 #include <assimp/material.h>
 #include <assimp/mesh.h>
-
-
 
 ModuleModelLoader::ModuleModelLoader()
 {
@@ -20,8 +18,21 @@ ModuleModelLoader::~ModuleModelLoader()
 
 
 bool ModuleModelLoader::Init() {
-	const aiScene* scene = aiImportFile("barker_house/BakerHouse", 0);
-	if (scene == NULL) App->menu->consoleLog(aiGetErrorString());
+	scene = aiImportFile("barker_house/BakerHouse.fbx", 0);
+	if (scene == nullptr) {
+		App->menu->consoleLog(aiGetErrorString());
+		LOG(aiGetErrorString());
+	}
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	for (unsigned i = 0; i < scene->mNumMeshes; ++i)
+	{
+		GenerateMeshData(scene->mMeshes[i]);
+	}
+	for (unsigned i = 0; i < scene->mNumMaterials; ++i)
+	{
+		GenerateMaterialData(scene->mMaterials[i]);
+	}
 	return true;
 }
 update_status   ModuleModelLoader::Update() {
@@ -29,4 +40,21 @@ update_status   ModuleModelLoader::Update() {
 }
 bool           ModuleModelLoader::CleanUp(){
 	return true;
+}
+
+void ModuleModelLoader::GenerateMeshData(aiMesh* mesh) {
+
+	
+	/*glBufferData(GL_ARRAY_BUFFER, mesh->mNumVertices * 5 * sizeof(unsigned int), nullptr, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, mesh->mNumVertices * 3 * sizeof(unsigned int), &mesh->mVertices);
+	glBufferSubData(GL_ARRAY_BUFFER, mesh->mNumVertices * 3 * sizeof(unsigned int), mesh->mNumVertices * 2 * sizeof(unsigned int), &mesh->mTextureCoords);
+	*/
+	//potser la mida es 5*3*sizeof
+	glBufferData(GL_ARRAY_BUFFER, mesh->mNumVertices * 3 * sizeof(unsigned int), nullptr, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, mesh->mNumVertices * 3 * sizeof(unsigned int), &mesh->mVertices);
+
+}
+
+void ModuleModelLoader::GenerateMaterialData(aiMaterial* mat) {
+
 }
--- ModuleModelLoader.h
+++ ModuleModelLoader.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Module.h"
+#include <assimp/scene.h>
 class ModuleModelLoader :
 	public Module
 {
@@ -7,8 +8,18 @@ public:
 	ModuleModelLoader();
 	~ModuleModelLoader();
 
+	void GenerateMeshData(aiMesh* mesh);
+	void GenerateMaterialData(aiMaterial* mat);
+
 	bool            Init();
 	update_status   Update();
 	bool            CleanUp();
+
+	//variables
+private:
+
+	//variables
+	const aiScene* scene;
+	unsigned vbo = 0;
 };
 
