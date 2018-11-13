6caa49d1445866a38d7c5abc23ddeb977b83873a
Roger Nogue Ballbé <rogernogueb@gmail.com>
2018-11-13 17:03:45 +0100

    gameobject class created

--- GameObject.cpp
+++ GameObject.cpp
@@ -1,12 +1,53 @@
 #include "GameObject.h"
+#include "ComponentMaterial.h"
+#include "ComponentCamera.h"
+#include "ComponentMesh.h"
+#include "ComponentTransform.h"
 
 
-
-GameObject::GameObject()
+GameObject::GameObject(char* n)
 {
+	active = false;
+	name = n;
 }
 
 
 GameObject::~GameObject()
 {
+}
+
+void GameObject::Update() {
+	for (int i = 0; i < components.size(); ++i) {
+		if (components[i]->active) {
+			components[i]->Update();
+		}
+	}
+}
+Component* GameObject::createComponent(component_type type) {
+	switch (type) {
+		case CAMERA:
+		{
+			ComponentCamera c(this);
+			components.push_back(&c);
+			break;
+		}
+		case MESH:
+		{
+			ComponentMesh c(this);
+			components.push_back(&c);
+			break;
+		}
+		case MATERIAL:
+		{
+			ComponentMaterial c(this);
+			components.push_back(&c);
+			break;
+		}
+		case TRANSFORM:
+		{
+			ComponentTransform c(this);
+			components.push_back(&c);
+			break;
+		}
+	}
 }
--- GameObject.h
+++ GameObject.h
@@ -1,9 +1,23 @@
 #ifndef __ModuleGameObject_H__
 #define __ModuleGameObject_H__
+
+#include "Globals.h"
+#include <vector>
+class Component;
+
 class GameObject
 {
 public:
-	GameObject();
+	GameObject(char* n);
 	~GameObject();
+
+	void Update();
+	Component* createComponent(component_type type);
+
+	//variables
+	bool active;
+	char* name;
+	std::vector<Component*> components;
+
 };
 #endif
