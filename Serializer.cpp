16527f8c6273151547d7bcc748ea91d5eb406b92
Roger Nogue Ballbé <rogernogueb@gmail.com>
2018-12-20 16:53:23 +0100

    some serializer functions implemented

--- Serializer.cpp
+++ Serializer.cpp
@@ -11,7 +11,158 @@ Serializer::~Serializer()
 {
 }
 
+JSON_File* Serializer::openReadJSON(const char* path) {
+	FILE* fp = fopen(path, "rb");
+	char readBuffer[65536];
 
-void Serializer::saveObject(GameObject* obj) {
+	if (fp != nullptr)
+		return new JSON_File(new rapidjson::FileReadStream(fp, readBuffer, sizeof(readBuffer)), fp);
+	else
+		return nullptr;
+}
 
+JSON_File* Serializer::openWriteJSON(const char* path) {
+	FILE* fp = fopen(path, "wb");
+	char writeBuffer[65536];
+
+	if (fp != nullptr)
+		return new JSON_File(new rapidjson::FileWriteStream(fp, writeBuffer, sizeof(writeBuffer)), fp);
+	else
+		return nullptr;
+}
+
+void Serializer::loadScene(const char* path, GameObject* newBase) {
+	JSON_File* json = openReadJSON(path);
+	//now we go over the json puting the values into the game object
+}
+
+void Serializer::saveScene(const char* path, GameObject* oldBase) {
+	JSON_File* json = openWriteJSON(path);
+	//now we go over the object writing in the json
+}
+
+//Structure functions
+JSON_File::JSON_File(rapidjson::FileWriteStream* os, FILE* fp) : os(os), fp(fp)
+{
+	document = new rapidjson::Document();
+	document->SetObject();
+
+	allocator = &document->GetAllocator();
+}
+
+JSON_File::JSON_File(rapidjson::FileReadStream* is, FILE* fp) : is(is), fp(fp)
+{
+	document = new rapidjson::Document();
+	document->ParseStream(*is);
+
+	allocator = &document->GetAllocator();
+}
+
+JSON_File::JSON_File()
+{
+	document = new rapidjson::Document();
+	document->SetObject();
+
+	allocator = &document->GetAllocator();
+}
+
+JSON_File::~JSON_File()
+{
+	int size = allocatedValues.size();
+	for (int i = 0; i < size; i++)
+	{
+		RELEASE(allocatedValues[i]);
+	}
+	allocatedValues.clear();
+
+	RELEASE(document);
+
+	RELEASE(is);
+	RELEASE(os);
+}
+
+bool JSON_File::Write()
+{
+	if (os != nullptr)
+	{
+		//rapidjson::Writer<rapidjson::FileWriteStream> writer(*os);
+		rapidjson::PrettyWriter<rapidjson::FileWriteStream> writer(*os);
+		document->Accept(writer);
+
+		return true;
+	}
+	else
+	{
+		if (is != nullptr)
+		{
+			LOG("Cannot write a read only file!");
+		}
+		else
+		{
+			LOG("Cannot write a virtual file!");
+		}
+
+		return false;
+	}
+}
+JSON_Value* JSON_File::createValue()
+{
+	JSON_Value* ret = new JSON_Value(allocator);
+	allocatedValues.push_back(ret);
+	return ret;
+}
+
+void JSON_File::addValue(const char * name, JSON_Value* value)
+{
+	std::string str = name;
+	rapidjson::Value index(str.c_str(), str.size(), *allocator);
+	document->AddMember(index, *value->getRapidJSONValue(), *allocator);
+}
+
+JSON_Value* JSON_File::getValue(const char* name)
+{
+	if (document->IsObject() && document->HasMember(name))
+	{
+		rapidjson::Value& value = document->operator[](name);
+		JSON_Value* ret = new JSON_Value(allocator);
+		allocatedValues.push_back(ret);
+		ret->getRapidJSONValue()->CopyFrom(value, *allocator, false);
+
+		return ret;
+	}
+
+	return nullptr;
+}
+
+void JSON_File::setValue(const char * name, JSON_Value * value)
+{
+	if (this->document->HasMember(name))
+		this->document->operator[](name) = *value->getRapidJSONValue(); //If it exists modify it
+	else
+		this->addValue(name, value); //if not, set a new one
+}
+
+JSON_Value::~JSON_Value()
+{
+	int size = allocatedValues.size();
+	for (int i = 0; i < size; i++)
+	{
+		RELEASE(allocatedValues[i]);//deletes buffer
+	}
+	allocatedValues.clear();
+
+	RELEASE(value);
+}
+
+void JSON_Value::convertToArray()
+{
+	RELEASE(value);
+	value = new rapidjson::Value(rapidjson::kArrayType);
+}
+
+void JSON_Value::addInt(const char* name, int value)
+{
+	std::string str = name;
+	rapidjson::Value index(str.c_str(), str.size(), *allocator);
+	this->value->AddMember(index, value, *allocator);
 }
--- Serializer.h
+++ Serializer.h
@@ -1,7 +1,96 @@
 #ifndef __Serializer_H__
 #define __Serializer_H__
 
+#include "rapidjson/rapidjson.h"
+#include "rapidjson/document.h"// rapidjson's DOM-style API
+#include "rapidjson/prettywriter.h"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/filereadstream.h"
+#include "MathGeoLib.h"
+
 class GameObject;
+
+struct JSON_Value
+{
+public:
+
+	JSON_Value(rapidjson::Document::AllocatorType* allocator) : allocator(allocator)
+	{
+		value = new rapidjson::Value(rapidjson::kObjectType);
+	}
+
+	~JSON_Value();
+
+	//This will remove all added elements of the JSON_Value
+	void convertToArray();
+
+	void addInt(const char* name, int value);
+	void addUint(const char* name, unsigned int value);
+	void addFloat(const char* name, float value);
+	void addString(const char* name, const char* value);
+	void addBool(const char* name, bool value);
+	void addVector2(const char* name, float2 vec);
+	void addVector3(const char* name, float3 vec);
+	void addQuat(const char* name, Quat quat);
+	void addTransform(const char* name, float4x4 mat);
+
+	void setUint(const char* name, unsigned int value);
+
+	int getInt(const char* name);
+	unsigned int getUint(const char* name);
+	float getFloat(const char* name);
+	const char* getString(const char* name);
+	bool getBool(const char* name);
+	float2 getVector2(const char* name);
+	float3 getVector3(const char* name);
+	Quat getQuat(const char* name);
+	float4x4 getTransform(const char* name);
+
+	JSON_Value* createValue();
+	void addValue(const char* name, JSON_Value* value);
+	JSON_Value* getValue(const char* name);
+	JSON_Value* getValueFromArray(int index);
+	void setValue(const char* name, JSON_Value* value);
+
+	void setValue(rapidjson::Value* value);
+	rapidjson::Value* getRapidJSONValue();
+
+private:
+
+	rapidjson::Value* value = nullptr;
+	rapidjson::Document::AllocatorType* allocator = nullptr;
+	std::vector<JSON_Value*> allocatedValues;
+};
+
+struct JSON_File
+{
+public:
+
+	JSON_File(rapidjson::FileWriteStream* os, FILE* fp);
+	JSON_File(rapidjson::FileReadStream* is, FILE* fp);
+	JSON_File();
+	~JSON_File();
+
+	bool Write();
+
+	JSON_Value* createValue();
+	void addValue(const char* name, JSON_Value* value);
+	JSON_Value* getValue(const char* name);
+	void setValue(const char* name, JSON_Value* value);
+
+	void closeFile();
+
+private:
+
+	FILE* fp = nullptr;
+
+	rapidjson::Document* document = nullptr;
+	rapidjson::FileWriteStream* os = nullptr;
+	rapidjson::FileReadStream* is = nullptr;
+
+	rapidjson::Document::AllocatorType* allocator = nullptr;
+	std::vector<JSON_Value*> allocatedValues;
+};
 
 class Serializer
 {
@@ -9,10 +98,12 @@ public:
 	Serializer();
 	~Serializer();
 
-	void saveObject(GameObject* obj);
+	void loadScene(const char* path, GameObject* newBase);
+	void saveScene(const char* path, GameObject* oldBase);
 
 private:
-
+	JSON_File* openReadJSON(const char* path);
+	JSON_File* openWriteJSON(const char* path);
 };
 
 #endif
